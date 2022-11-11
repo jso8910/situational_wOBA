88cbfa0f0c56267f54dd3f428416abde156e6660
jso8910 <mail4jasonr@gmail.com>
2022-11-11 14:26:55 +0800

    Made gen_weights.py main function

--- gen_weights.py
+++ gen_weights.py
@@ -64,7 +64,7 @@ def base_outs_weights(plays: list[Dict[str, str]]) -> list[list[str | int]]:
 
     for idx in range(len(avg_outcomes)):
         for idx_inner in range(2, len(avg_outcomes[idx])):
-            avg_outcomes[idx][idx_inner] = {"first": {"total": 0, "average": 0}, "second": {"total": 0, "average": 0}, "third": {
+            avg_outcomes[idx][idx_inner] = {"first": {"total": 0, "average": 0}, "second": {"total": 0, "average": 0}, "third": { # type: ignore
                 "total": 0, "average": 0}, "outs": {"total": 0, "average": 0}, "run_exp_after": {"total": 0.0, "average": 0.0}, "runs_on_play": {"total": 0.0, "average": 0.0}, "n_scenarios": 0}
 
     for play in tqdm.tqdm(plays):
@@ -80,31 +80,24 @@ def base_outs_weights(plays: list[Dict[str, str]]) -> list[list[str | int]]:
         end_sit_idx = end_base_state * 3 + end_outs
         situation = avg_outcomes[sit_idx]
         outcome: dict[str, int | dict[str, int]
-                      ] = situation[event_outcome_idx]
+                      ] = situation[event_outcome_idx] # type: ignore
 
-        outcome['first']['total'] += bool(end_base_state & 0b001)
-        outcome['second']['total'] += bool(end_base_state & 0b010)
-        outcome['third']['total'] += bool(end_base_state & 0b100)
-        outcome['outs']['total'] += int(play["OUTS_CT"]) + \
-            int(play["EVENT_OUTS_CT"])
-        outcome['run_exp_after']['total'] += run_exp_by_sit[end_sit_idx][-1] if end_outs < 3 else 0
-        outcome['runs_on_play']['total'] += int(play["EVENT_RUNS_CT"])
-        outcome['n_scenarios'] += 1
+        outcome['first']['total'] += bool(end_base_state & 0b001) # type: ignore
+        outcome['second']['total'] += bool(end_base_state & 0b010) # type: ignore
+        outcome['third']['total'] += bool(end_base_state & 0b100) # type: ignore
+        outcome['outs']['total'] += int(play["OUTS_CT"]) + int(play["EVENT_OUTS_CT"]) # type: ignore
+        outcome['run_exp_after']['total'] += run_exp_by_sit[end_sit_idx][-1] if end_outs < 3 else 0 # type: ignore
+        outcome['runs_on_play']['total'] += int(play["EVENT_RUNS_CT"]) # type: ignore
+        outcome['n_scenarios'] += 1 # type: ignore
 
     for idx in range(len(avg_outcomes)):
         for idx_inner in range(2, len(avg_outcomes[idx])):
-            avg_outcomes[idx][idx_inner]['first']['average'] = avg_outcomes[idx][idx_inner]['first']['total'] / \
-                avg_outcomes[idx][idx_inner]['n_scenarios']
-            avg_outcomes[idx][idx_inner]['second']['average'] = avg_outcomes[idx][idx_inner]['second']['total'] / \
-                avg_outcomes[idx][idx_inner]['n_scenarios']
-            avg_outcomes[idx][idx_inner]['third']['average'] = avg_outcomes[idx][idx_inner]['third']['total'] / \
-                avg_outcomes[idx][idx_inner]['n_scenarios']
-            avg_outcomes[idx][idx_inner]['outs']['average'] = avg_outcomes[idx][idx_inner]['outs']['total'] / \
-                avg_outcomes[idx][idx_inner]['n_scenarios']
-            avg_outcomes[idx][idx_inner]['run_exp_after']['average'] = avg_outcomes[idx][idx_inner]['run_exp_after']['total'] / \
-                avg_outcomes[idx][idx_inner]['n_scenarios']
-            avg_outcomes[idx][idx_inner]['runs_on_play']['average'] = avg_outcomes[idx][idx_inner]['runs_on_play']['total'] / \
-                avg_outcomes[idx][idx_inner]['n_scenarios']
+            avg_outcomes[idx][idx_inner]['first']['average'] = avg_outcomes[idx][idx_inner]['first']['total'] / avg_outcomes[idx][idx_inner]['n_scenarios'] # type: ignore
+            avg_outcomes[idx][idx_inner]['second']['average'] = avg_outcomes[idx][idx_inner]['second']['total'] / avg_outcomes[idx][idx_inner]['n_scenarios'] # type: ignore
+            avg_outcomes[idx][idx_inner]['third']['average'] = avg_outcomes[idx][idx_inner]['third']['total'] / avg_outcomes[idx][idx_inner]['n_scenarios'] # type: ignore
+            avg_outcomes[idx][idx_inner]['outs']['average'] = avg_outcomes[idx][idx_inner]['outs']['total'] / avg_outcomes[idx][idx_inner]['n_scenarios'] # type: ignore
+            avg_outcomes[idx][idx_inner]['run_exp_after']['average'] = avg_outcomes[idx][idx_inner]['run_exp_after']['total'] / avg_outcomes[idx][idx_inner]['n_scenarios'] # type: ignore
+            avg_outcomes[idx][idx_inner]['runs_on_play']['average'] = avg_outcomes[idx][idx_inner]['runs_on_play']['total'] / avg_outcomes[idx][idx_inner]['n_scenarios'] # type: ignore
 
     run_exp = [
         # ["Outs", "Base State", "1B",
@@ -119,8 +112,7 @@ def base_outs_weights(plays: list[Dict[str, str]]) -> list[list[str | int]]:
         # We don't want to consider leverage index
         for idx_inner in range(2, len(run_exp[idx]) - 1):
             # (Runs scored + New run expectancy) - existing run expectancy
-            run_exp[idx][idx_inner] += (avg_outcomes[idx][idx_inner]["run_exp_after"]["average"] + avg_outcomes[idx][idx_inner]['runs_on_play']['average']) - \
-                (run_exp_by_sit[idx][-1])
+            run_exp[idx][idx_inner] += (avg_outcomes[idx][idx_inner]["run_exp_after"]["average"] + avg_outcomes[idx][idx_inner]['runs_on_play']['average']) - (run_exp_by_sit[idx][-1]) # type: ignore
             # Divide by leverage index
             run_exp[idx][idx_inner] /= run_exp[idx][-1]
             # Subtract value of the out (value of out is negative)
@@ -160,70 +152,73 @@ def base_outs_weights(plays: list[Dict[str, str]]) -> list[list[str | int]]:
     }
 
     for idx in range(len(numerators)):
-        numerators[idx][2] = (avg_batting_line["Hits"] -
+        numerators[idx][2] = (avg_batting_line["Hits"] - # type: ignore
                               avg_batting_line["2B"] - avg_batting_line["3B"] - avg_batting_line["HR"])*run_exp[idx][2] + avg_batting_line["2B"]*run_exp[idx][3] + \
             avg_batting_line["3B"]*run_exp[idx][4] + avg_batting_line["HR"]*run_exp[idx][5] + avg_batting_line["UBB"]*run_exp[idx][6] + \
             avg_batting_line["HBP"]*run_exp[idx][7] + \
             avg_batting_line["K"]*run_exp[idx][8]
         # Based on stats above (not rate stats, they're messed up), hitters get on base 189 times per 600 PA
-        numerators[idx][3] = 189/numerators[idx][2]
+        numerators[idx][3] = 189/numerators[idx][2] # type: ignore
         # Don't include 2 inputs and LI
         for idx_inner in range(2, len(run_exp[idx]) - 1):
             run_exp[idx][idx_inner] *= numerators[idx][3]
 
     # Header
-    run_exp.insert(0, ["Outs State", "Base State", "1B", "2B",
+    run_exp.insert(0, ["Outs State", "Base State", "1B", "2B", # type: ignore
                    "3B", "HR", "UBB", "HBP", "K", "Out", "LI"])
 
-    return [row[:-1] for row in run_exp]
+    return [row[:-1] for row in run_exp] # type: ignore
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--start-year', '-s',
-                    help="Start year of data gathering (defaults to 2000 for a somewhat reasonable default)", type=int, default=2000)
-parser.add_argument('--end-year', '-e',
-                    help="End year of data gathering (defaults to 2021, current retrosheet year as of coding)", type=int, default=2021)
-parser.add_argument('--use-innings', '-i',
-                    help="Use innings rather than just base-outs in weights. The output file is named differently if you use innings", type=int, default=False)
+def main(start_year: int, end_year: int, use_innings: bool):
+    if start_year > end_year:
+        print("START_YEAR must be less than END_YEAR", file=sys.stderr)
+        sys.exit(1)
+    elif start_year < 1915 or end_year > 2021:
+        print("START_YEAR and END_YEAR must be between 1915 and 2021. If 2022 or a future year has been added to retrosheet, feel free to edit this file.", file=sys.stderr)
+        sys.exit(1)
 
-args = parser.parse_args(sys.argv[1:])
+    if not os.path.isdir("chadwick_csv"):
+        print("The folder chadwick_csv doesn't exist. Have you run retrosheet_to_csv.sh?", file=sys.stderr)
+        sys.exit(1)
+    files = sorted(os.listdir("chadwick_csv"))
+    if not len(files):
+        print("The folder chadwick_csv doesn't have any folders. Have you run retrosheet_to_csv.sh?", file=sys.stderr)
+        sys.exit(1)
 
-if args.start_year > args.end_year:
-    print("START_YEAR must be less than END_YEAR", file=sys.stderr)
-    sys.exit(1)
-elif args.start_year < 1915 or args.end_year > 2021:
-    print("START_YEAR and END_YEAR must be between 1915 and 2021. If 2022 or a future year has been added to retrosheet, feel free to edit this file.", file=sys.stderr)
-    sys.exit(1)
+    files_filtered = []
+    for file in files:
+        if int(file[0:4]) < start_year or int(file[0:4]) > end_year:
+            continue
+        else:
+            files_filtered.append(file) # type: ignore
+    plays: list[Dict[str, str]] = []
 
-if not os.path.isdir("chadwick_csv"):
-    print("The folder chadwick_csv doesn't exist. Have you run retrosheet_to_csv.sh?", file=sys.stderr)
-    sys.exit(1)
-files = sorted(os.listdir("chadwick_csv"))
-if not len(files):
-    print("The folder chadwick_csv doesn't have any folders. Have you run retrosheet_to_csv.sh?", file=sys.stderr)
-    sys.exit(1)
+    for file in tqdm.tqdm(files_filtered): # type: ignore
+        if int(file[0:4]) < start_year or int(file[0:4]) > end_year: # type: ignore
+            continue
+        file = "chadwick_csv/" + file # type: ignore
+        with open(file, "r") as f: # type: ignore
+            reader = csv.DictReader(f)
+            plays.extend(list(reader))
 
-files_filtered = []
-for file in files:
-    if int(file[0:4]) < args.start_year or int(file[0:4]) > args.end_year:
-        continue
+    if use_innings:
+        raise NotImplementedError()
     else:
-        files_filtered.append(file)
-plays: list[Dict[str, str]] = []
+        weights = base_outs_weights(plays)
+        with open("base_out_weights.csv", "w") as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+            [writer.writerow(row) for row in weights]
+        print("Outputted weights to base_out_weights.csv")
 
-for file in tqdm.tqdm(files_filtered):
-    if int(file[0:4]) < args.start_year or int(file[0:4]) > args.end_year:
-        continue
-    file = "chadwick_csv/" + file
-    with open(file, "r") as f:
-        reader = csv.DictReader(f)
-        plays.extend(list(reader))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start-year', '-s',
+                        help="Start year of data gathering (defaults to 2000 for a somewhat reasonable default)", type=int, default=2000)
+    parser.add_argument('--end-year', '-e',
+                        help="End year of data gathering (defaults to 2021, current retrosheet year as of coding)", type=int, default=2021)
+    parser.add_argument('--use-innings', '-i',
+                        help="Use innings rather than just base-outs in weights. The output file is named differently if you use innings", type=int, default=False)
 
-if args.use_innings:
-    raise NotImplementedError()
-else:
-    weights = base_outs_weights(plays)
-    with open("base_out_weights.csv", "w") as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-        [writer.writerow(row) for row in weights]
-    print("Outputted weights to base_out_weights.csv")
+    args = parser.parse_args(sys.argv[1:])
+    main(args.start_year, args.end_year, args.use_innings)
